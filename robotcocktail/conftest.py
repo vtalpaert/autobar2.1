157c0464ade825c14c8f0640218b8f6ad908d0a0
TALPAERT Victor <victor.talpaert@akka.eu>
2023-01-29 15:14:24 +0100

    Project creation with cockiecutter

---
+++ robotcocktail/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from robotcocktail.users.models import User
+from robotcocktail.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
